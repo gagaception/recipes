99c1a268d9bcf3205812bf6eee77367e91cb2297
gagaception <sharipov.aziz.sh@gmail.com>
2021-03-15 13:05:57 +0600

    Added tests for BaseService

---
+++ spec/services/base_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe BaseService, type: :service do
+  context "Response" do
+    it "should be a Struct" do
+      expect(BaseService::Response.new).to be_a(Struct)
+    end
+    
+    it "should respond to successful?" do
+      expect(BaseService::Response.new).to respond_to(:successful?)
+    end
+
+    it "should respond to error" do
+      expect(BaseService::Response.new).to respond_to(:error)
+    end
+    it "should respond to data" do
+      expect(BaseService::Response.new).to respond_to(:data)
+    end
+  end
+end
